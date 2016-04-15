babfb63e8500b7d0277d26e8f2c0beed79e0abbb
pbarragan <patrick.r.barragan@gmail.com>
2016-04-15 16:10:50 -0400

    saving to switch back to dev 2

---
+++ models/doctor.js
@@ -0,0 +1,39 @@
+// app/models/user.js
+// load the things we need
+var mongoose = require('mongoose');
+//var bcrypt   = require('bcrypt-nodejs');
+
+// define the schema for our user model
+var doctorSchema = mongoose.Schema({
+
+    name: String,
+    numandstreet: String,
+    city: String,
+    state: String,
+    zip: String,
+    phone: String,
+    url: String,
+    procedures: [String],
+    prices: [Number],
+    percents: [Number],
+    stars: Number,
+    imgurl: String
+
+});
+
+/*
+// methods ======================
+// generating a hash
+doctorSchema.methods.generateHash = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+// checking if password is valid
+doctorSchema.methods.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.local.password);
+};
+*/
+
+// create the model for users and expose it to our app
+module.exports = mongoose.model('Doctor', doctorSchema);
+
---
+++ src/DoctorInputC.js
@@ -0,0 +1,98 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var $ = require('jquery');
+var auth = require('./../app/authentication');
+
+var Input = require('react-bootstrap/lib/Input');
+var Button  = require('react-bootstrap/lib/Button');
+var ButtonInput  = require('react-bootstrap/lib/ButtonInput');
+
+var SignupC = React.createClass({
+	contextTypes: {
+    router: React.PropTypes.object.isRequired
+  	},
+	getInitialState: function() {
+    	return {
+      	error: false,
+      	errorMessage: ''
+    	}
+  	},
+	submitSignup: function(e){
+    	console.log('I am trying to sign up');
+      e.preventDefault();
+
+    	//console.log(this.refs.email.getValue());
+    	//console.log(this.refs.password.getValue());
+
+		if(this.refs.password.getValue() === this.refs.confirm.getValue()){
+			if(this.refs.password.getValue().length > 0){
+    			auth.signup(this.refs.email.getValue(), 
+    				this.refs.password.getValue(), 
+    				(signupWorked,message) => {
+      				if (!signupWorked) console.log("Signup Failed");
+      				else console.log("Signup succeeded");
+
+      			if (!signupWorked)
+        			return this.setState({ error: true , errorMessage: message})
+
+      			const { location } = this.props
+      			console.log(location);
+      			console.log(location.state);
+      			console.log('WHY IS NOTHING WORKING!!!!');
+      			//console.log(location.state.nextPathname);
+      			if (location.state && location.state.nextPathname) {
+      				console.log('i am in the nextPathName');
+      				this.context.router.replace(location.state.nextPathname)
+      			} else {
+      				console.log('i am in the index');
+      				this.context.router.replace('/index')
+      			}
+      		});
+
+    			console.log('got past the signup call');
+    		}
+    		else{
+    			this.setState({ error: true, 
+    			errorMessage: "Oops! Your password is too short." });
+    		}
+    	}
+    	else{
+    		this.setState({ error: true, 
+    			errorMessage: "Oops! Please make sure your confirmed password matches." });
+    	}
+  	},
+	render: function(){
+
+    var procedureList = ["Liposuction","Eye lift","Rhinoplasty","Facelift",
+    "Brow Lift","Chin Augmentation","Injectables","Tummy Tuck",
+    "Breast augmentation","Breast Lift","Breast Reduction",
+    "Breast Reconstruction","Brazilian butt lift","Mommy Makeover",
+    "Arm Lift","Body Lift"];
+
+    var procChecks = procedureList.map(function(proc,i) {
+      return <Input type="checkbox" label=proc key={i} />
+    });
+
+		return (
+      <div><h1>Sign up for a free account</h1>
+			<form onSubmit={this.submitSignup}>
+            <Input type="email" ref="email" placeholder="Email" required/>
+            <Input type="password" ref="password" placeholder="Password" required/>
+            <Input type="password" ref="confirm" placeholder="Confirm Password" required/>
+            {procChecks}
+            <div>
+            	<ButtonInput type="submit" value="Sign Up"
+            		bsSize="large" />
+            </div>
+            <div style={{color:"red"}}>
+            {this.state.error ? this.state.errorMessage : ''}
+            </div>
+          	</form></div>
+
+		);
+	}
+
+});
+
+//{this.state.error ? this.state.errorMessage : 'No error'}
+module.exports = SignupC;
